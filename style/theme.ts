6c752e8f5ad29002bc9ab135295368ee71bfaf5d
Andy Hook <hello@andy-hook.co.uk>
2021-01-08 17:03:24 +0000

    Support alpha values in theme

--- style/theme.ts
+++ style/theme.ts
@@ -1,4 +1,4 @@
-import { hsl } from 'polished'
+import { hsla } from 'polished'
 import { css, CSSProp, DefaultTheme, ThemeProps } from 'styled-components'
 import { BreakpointList, breakpoints } from './responsive'
 
@@ -8,25 +8,30 @@ type LimitedShadeRange = 'low' | 'medium' | 'high'
 type FullShadeRange = LimitedShadeRange | 'extraLow' | 'extraHigh'
 type ColourRange = 'base' | 'light' | 'dark'
 
-export type Theme = {
-  name: ThemeName
-  foreground: Record<FullShadeRange, string>
-  background: Record<LimitedShadeRange, string>
-  accent: Record<ColourRange, string>
-  positive: Record<ColourRange, string>
+type ColorValue = [number, number, number]
+
+type ThemeCommon = {
+  accent: Record<ColourRange, ColorValue>
+  positive: Record<ColourRange, ColorValue>
   breakpoints: BreakpointList
 }
 
-const common = {
+export type Theme = {
+  name: ThemeName
+  foreground: Record<FullShadeRange, ColorValue>
+  background: Record<LimitedShadeRange, ColorValue>
+} & ThemeCommon
+
+const common: ThemeCommon = {
   accent: {
-    base: hsl(16, 0.96, 0.69),
-    light: hsl(354, 0.89, 0.64),
-    dark: hsl(346, 0.91, 0.56),
+    base: [16, 0.96, 0.69],
+    light: [354, 0.89, 0.64],
+    dark: [346, 0.91, 0.56],
   },
   positive: {
-    base: hsl(115, 0.64, 0.5),
-    light: hsl(115, 0.64, 0.65),
-    dark: hsl(115, 0.65, 0.3),
+    base: [115, 0.64, 0.5],
+    light: [115, 0.64, 0.65],
+    dark: [115, 0.65, 0.3],
   },
   breakpoints: breakpoints,
 }
@@ -36,16 +41,16 @@ const common = {
 export const lightTheme: Theme = {
   name: 'light',
   foreground: {
-    extraLow: hsl(206, 0.08, 0.9),
-    low: hsl(206, 0.08, 0.85),
-    medium: hsl(206, 0.1, 0.5),
-    high: hsl(206, 0.11, 0.35),
-    extraHigh: hsl(215, 0.13, 0.02),
+    extraLow: [206, 0.08, 0.9],
+    low: [206, 0.08, 0.85],
+    medium: [206, 0.1, 0.5],
+    high: [206, 0.11, 0.35],
+    extraHigh: [215, 0.13, 0.02],
   },
   background: {
-    low: hsl(223, 0.16, 0.95),
-    medium: hsl(220, 0.16, 0.96),
-    high: hsl(220, 0.16, 0.98),
+    low: [223, 0.16, 0.95],
+    medium: [220, 0.16, 0.96],
+    high: [220, 0.16, 0.98],
   },
   ...common,
 }
@@ -55,16 +60,16 @@ export const lightTheme: Theme = {
 export const darkTheme: Theme = {
   name: 'dark',
   foreground: {
-    extraLow: hsl(215, 0.13, 0.2),
-    low: hsl(206, 0.11, 0.35),
-    medium: hsl(206, 0.1, 0.55),
-    high: hsl(206, 0.8, 0.62),
-    extraHigh: hsl(0, 0, 1),
+    extraLow: [215, 0.13, 0.2],
+    low: [206, 0.11, 0.35],
+    medium: [206, 0.1, 0.55],
+    high: [206, 0.8, 0.62],
+    extraHigh: [0, 0, 1],
   },
   background: {
-    low: hsl(206, 0.13, 0.11),
-    medium: hsl(207, 0.13, 0.14),
-    high: hsl(204, 0.13, 0.15),
+    low: [206, 0.13, 0.11],
+    medium: [207, 0.13, 0.14],
+    high: [204, 0.13, 0.15],
   },
   ...common,
 }
@@ -74,26 +79,55 @@ export const themes = {
   dark: darkTheme,
 }
 
-export function applyForeground(value: keyof Theme['foreground']): CSSProp {
+function applyHsl(value: ColorValue, alpha?: number): string {
+  const [hue, saturation, lightness] = value
+
+  const props = {
+    hue,
+    saturation,
+    lightness,
+    alpha: alpha !== undefined && alpha < 1 ? alpha : 1,
+  }
+
+  return hsla(props)
+}
+
+export function applyForeground(
+  value: keyof Theme['foreground'],
+  alpha?: number
+): CSSProp {
   return css`
-    ${({ theme }: ThemeProps<DefaultTheme>): string => theme.foreground[value]}
+    ${({ theme }: ThemeProps<DefaultTheme>): string =>
+      applyHsl(theme.foreground[value], alpha)}
   `
 }
 
-export function applyBackground(value: keyof Theme['background']): CSSProp {
+export function applyBackground(
+  value: keyof Theme['background'],
+  alpha?: number
+): CSSProp {
   return css`
-    ${({ theme }: ThemeProps<DefaultTheme>): string => theme.background[value]}
+    ${({ theme }: ThemeProps<DefaultTheme>): string =>
+      applyHsl(theme.background[value], alpha)}
   `
 }
 
-export function applyAccent(value: keyof Theme['accent']): CSSProp {
+export function applyAccent(
+  value: keyof Theme['accent'],
+  alpha?: number
+): CSSProp {
   return css`
-    ${({ theme }: ThemeProps<DefaultTheme>): string => theme.accent[value]}
+    ${({ theme }: ThemeProps<DefaultTheme>): string =>
+      applyHsl(theme.accent[value], alpha)}
   `
 }
 
-export function applyPositive(value: keyof Theme['positive']): CSSProp {
+export function applyPositive(
+  value: keyof Theme['positive'],
+  alpha?: number
+): CSSProp {
   return css`
-    ${({ theme }: ThemeProps<DefaultTheme>): string => theme.positive[value]}
+    ${({ theme }: ThemeProps<DefaultTheme>): string =>
+      applyHsl(theme.positive[value], alpha)}
   `
 }
