aeaf1b65859b662987fceaa530dbcb6b19461052
Andy Hook <hello@andy-hook.co.uk>
2021-01-09 15:32:30 +0000

    Fix test

--- style/responsive.spec.tsx
+++ style/responsive.spec.tsx
@@ -2,14 +2,14 @@ import { uniformScale } from './responsive'
 
 describe('uniformScale', () => {
   test('should return correct value when supplied PX unit', () => {
-    expect(uniformScale('10px', 'xxl')).toMatch('0.45454545454545453vw')
+    expect(uniformScale('10px', 'xxl')).toMatch('0.47619047619047616vw')
   })
 
   test('should return correct value when supplied EM unit', () => {
-    expect(uniformScale('10em', 'xxl')).toMatch('7.2727272727272725vw')
+    expect(uniformScale('10em', 'xxl')).toMatch('7.619047619047619vw')
   })
 
   test('should return correct value when supplied REM unit', () => {
-    expect(uniformScale('10rem', 'xxl')).toMatch('7.2727272727272725vw')
+    expect(uniformScale('10rem', 'xxl')).toMatch('7.619047619047619vw')
   })
 })
