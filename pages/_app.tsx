85af09634a1042774401adb64498aaf4d86da32f
Andy Hook <hello@andyhook.dev>
2021-01-12 11:19:37 +0000

    Unregister service worker (#32)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import GlobalStyle from '../style/global'
 import { FocusVisibleProvider } from '../hooks/useFocusVisible/useFocusVisible'
-import { ThemeProvider, useTheme } from '../hooks/useTheme/useTheme'
+import { ThemeProvider } from '../hooks/useTheme/useTheme'
 import MetaBrowser from '../components/Meta/MetaBrowser'
 import MetaIcons from '../components/Meta/MetaIcons'
 import { darkTheme } from '../style/theme'
 
 // Import fonts outside of styled-components to avoid flicker on state change
 import '../style/font.css'
+import { unregisterServiceWorker } from '../serviceWorker'
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
+  // Remove any previously installed service worker
+  // This resolves an issue with stale cache from previous gatsby project
+  useEffect(() => {
+    unregisterServiceWorker()
+  }, [])
+
   return (
     <>
       <MetaBrowser />
@@ -18,27 +25,11 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
       <FocusVisibleProvider>
         <ThemeProvider theme={darkTheme}>
           <GlobalStyle />
-          <Body>
-            <Component {...pageProps} />
-          </Body>
+
+          <Component {...pageProps} />
         </ThemeProvider>
       </FocusVisibleProvider>
     </>
-  )
-}
-
-function Body({ children }: { children: React.ReactNode }): JSX.Element {
-  const { background } = useTheme()
-
-  return (
-    <div
-      css={`
-        // Set initial background to avoid possible colour flash
-        background-color: ${background('low')};
-      `}
-    >
-      {children}
-    </div>
   )
 }
 
