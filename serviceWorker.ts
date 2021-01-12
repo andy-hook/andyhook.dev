28f64ad29794757519c19f03d7b677a6f3a4f432
Andy Hook <hello@andy-hook.co.uk>
2021-01-12 11:17:43 +0000

    Unregister service worker

---
+++ serviceWorker.ts
@@ -0,0 +1,11 @@
+export function unregisterServiceWorker(): void {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        registration.unregister()
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }
+}
