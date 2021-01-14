b877c4a9ff620839aa2cc6b70f80614449da01fb
Andy Hook <hello@andy-hook.co.uk>
2021-01-14 21:20:51 +0000

    Minor copy adjust

--- components/ExpList/ExpList.tsx
+++ components/ExpList/ExpList.tsx
@@ -3,7 +3,7 @@ import ExpListItem from './ExpListItem'
 
 const EXPERIENCE_ENTRIES = [
   ['2020', 'Aragon', 'Senior UI Engineer'],
-  ['2018', 'Bright Interactive', 'UI Developer'],
+  ['2018', 'Bright Interactive', 'UI Engineer'],
   ['2016', 'Brandwatch', 'Senior Front-End Developer'],
   ['2012', 'Jamieson Consultancy', 'Front-End Developer'],
   ['2009', 'Freelance', 'Digital Designer'],
