2e0e2b94e0e2a3e1667f097183ae09bf279e17bb
Andy Hook <hello@andy-hook.co.uk>
2021-01-14 21:25:38 +0000

    Remove redundant code

--- style/theme.ts
+++ style/theme.ts
@@ -135,22 +135,3 @@ export function applyHsl(value: ColorValue, alpha?: number): string {
 
   return hsla(props)
 }
-
-import { rgba } from 'polished'
-
-const shadowTint = '#1a424b'
-
-export const shadowDepth = {
-  low: `0px 1px 2px ${rgba(shadowTint, 0.125)}, 0px 2px 20px ${rgba(
-    shadowTint,
-    0.05
-  )}`,
-  medium: `0px 2px 4px ${rgba(shadowTint, 0.03)}, 0px 10px 20px ${rgba(
-    shadowTint,
-    0.06
-  )}`,
-  high: `0px 5px 10px ${rgba(shadowTint, 0.03)}, 0px 15px 50px ${rgba(
-    shadowTint,
-    0.08
-  )}`,
-}
