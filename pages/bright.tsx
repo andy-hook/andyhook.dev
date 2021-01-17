38c1b84ceed9eb14f6be75d8ce507b4953351ea1
Andy Hook <hello@andy-hook.co.uk>
2021-01-17 18:13:23 +0000

    Minor copy change

--- pages/bright.tsx
+++ pages/bright.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import WorkTemplate from '../components/WorkTemplate/WorkTemplate'
 
 function Bright(): JSX.Element {
-  return <WorkTemplate name="bright">Dash</WorkTemplate>
+  return <WorkTemplate name="bright">Bright</WorkTemplate>
 }
 
 export default Bright
