c5a2a7ba4bc4dc26863fc205158420b742b6d229
Andy Hook <hello@andy-hook.co.uk>
2021-02-03 14:52:33 +0000

    Home: Improve background image

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -10,158 +10,97 @@ import HireButton from '../HireButton/HireButton'
 import LayoutGutter from '../Layout/LayoutGutter'
 import LayoutLimiter from '../Layout/LayoutLimiter'
 import RemoveWidow from '../RemoveWidow/RemoveWidow'
-import ImageBase from '../ImageBase/ImageBase'
 import GradientText from '../GradientText/GradientText'
 
 function Hero(): JSX.Element {
-  const { foreground, background } = useTheme()
+  const { foreground } = useTheme()
 
   return (
-    <div
+    <LayoutGutter
       css={`
         position: relative;
+        z-index: ${appearance.index.low};
       `}
     >
-      <LayoutGutter
+      <LayoutLimiter
+        size="large"
         css={`
-          position: relative;
-          z-index: ${appearance.index.low};
+          display: flex;
+          flex-direction: column;
         `}
       >
-        <LayoutLimiter
-          size="large"
+        <div
           css={`
             display: flex;
-            flex-direction: column;
+            justify-content: center;
+
+            height: 100%;
+            flex: 1;
+
+            padding-top: 14rem;
+            padding-bottom: 7rem;
+
+            ${inclusiveUp('md')} {
+              padding-top: 20rem;
+              padding-bottom: 14rem;
+            }
           `}
         >
-          <div
+          <motion.div
+            variants={slideInMotion}
+            initial="offset"
+            animate="rest"
+            custom={{ offset: 100, delay: 0.75 }}
             css={`
-              display: flex;
-              justify-content: center;
-
-              height: 100%;
-              flex: 1;
-
-              padding-top: 14rem;
-              padding-bottom: 7rem;
-
-              ${inclusiveUp('md')} {
-                padding-top: 20rem;
-                padding-bottom: 14rem;
-              }
+              text-align: center;
             `}
           >
-            <motion.div
-              variants={slideInMotion}
-              initial="offset"
-              animate="rest"
-              custom={{ offset: 100, delay: 0.75 }}
+            <h1
               css={`
-                text-align: center;
-              `}
-            >
-              <h1
-                css={`
-                  ${displayText.weight.semiBold}
-                  ${displayText.size.lg}
+                ${displayText.weight.semiBold}
+                ${displayText.size.lg}
                   ${setCropAndLineHeight('display', 'tight')}
                   
                   max-width: 16em;
 
-                  margin-bottom: 1.25em;
+                margin-bottom: 1.25em;
+              `}
+            >
+              <GradientText
+                css={`
+                  position: relative;
+                  z-index: ${appearance.index.floor};
+                  ${displayText.weight.bold}
                 `}
               >
-                <GradientText
-                  css={`
-                    position: relative;
-                    z-index: ${appearance.index.floor};
-                    ${displayText.weight.bold}
-                  `}
-                >
-                  Senior UI Engineer
-                </GradientText>{' '}
-                <span
-                  css={`
+                Senior UI Engineer
+              </GradientText>{' '}
+              <span
+                css={`
                     position: relative;
                     z-index: ${appearance.index.low};
                     color ${foreground('high')};
                     text-shadow: ${appearance.textShadow.heavy};
                   `}
+              >
+                building{' '}
+                <span
+                  css={`
+                    ${inclusiveUp('xs')} {
+                      white-space: nowrap;
+                    }
+                  `}
                 >
-                  building{' '}
-                  <span
-                    css={`
-                      ${inclusiveUp('xs')} {
-                        white-space: nowrap;
-                      }
-                    `}
-                  >
-                    next-generation
-                  </span>{' '}
-                  <RemoveWidow>
-                    user interfaces out of Brighton, UK.
-                  </RemoveWidow>
-                </span>
-              </h1>
-              <HireButton href={`mailto:${meta.email}`} />
-            </motion.div>
-          </div>
-        </LayoutLimiter>
-      </LayoutGutter>
-
-      <div
-        css={`
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          z-index: ${appearance.index.floor};
-        `}
-      >
-        <div
-          css={`
-            position: absolute;
-            background: linear-gradient(
-              ${background('low', 0.6)} 0%,
-              ${background('low', 0)} 70%
-            );
-
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 20%;
-            z-index: ${appearance.index.low};
-          `}
-        />
-        <div
-          css={`
-            position: absolute;
-            background: linear-gradient(
-              ${background('low', 0)} 0%,
-              ${background('low', 1)} 100%
-            );
-
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 30%;
-            z-index: ${appearance.index.low};
-          `}
-        />
-        <ImageBase
-          src="/images/home-bg.jpg"
-          backgroundColor={background('low')}
-          width={6059}
-          height={5941}
-          quality={60}
-          css={`
-            opacity: 0.04;
-            z-index: ${appearance.index.floor};
-          `}
-        />
-      </div>
-    </div>
+                  next-generation
+                </span>{' '}
+                <RemoveWidow>user interfaces out of Brighton, UK.</RemoveWidow>
+              </span>
+            </h1>
+            <HireButton href={`mailto:${meta.email}`} />
+          </motion.div>
+        </div>
+      </LayoutLimiter>
+    </LayoutGutter>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import About from '../components/About/About'
 import Hero from '../components/Hero/Hero'
+import ImageBase from '../components/ImageBase/ImageBase'
 import MetaSocial from '../components/Meta/MetaSocial'
 import WorkGrid from '../components/WorkGrid/WorkGrid'
+import { useTheme } from '../hooks/useTheme/useTheme'
+import { appearance } from '../style/appearance'
 
 function Home(): JSX.Element {
+  const { background } = useTheme()
+
   return (
     <>
       <MetaSocial
@@ -12,8 +17,74 @@ function Home(): JSX.Element {
         description="Brighton based software developer specialising in high-performance
         UI Engineering"
       />
-      <Hero />
-      <WorkGrid />
+      <div
+        css={`
+          position: relative;
+          overflow: hidden;
+        `}
+      >
+        <div
+          css={`
+            position: relative;
+            z-index: ${appearance.index.medium};
+          `}
+        >
+          <Hero />
+          <WorkGrid />
+        </div>
+        <div
+          css={`
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            z-index: ${appearance.index.floor};
+          `}
+        >
+          <div
+            css={`
+              position: absolute;
+              background: linear-gradient(
+                ${background('low', 0.8)} 0%,
+                ${background('low', 0)} 70%
+              );
+
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 30%;
+              z-index: ${appearance.index.low};
+            `}
+          />
+
+          <ImageBase
+            src="/images/home-bg.jpg"
+            backgroundColor={background('low')}
+            width={2606}
+            height={3892}
+            quality={60}
+            css={`
+              opacity: 0.07;
+              z-index: ${appearance.index.floor};
+            `}
+          />
+        </div>
+        <div
+          css={`
+            position: absolute;
+            background: linear-gradient(
+              ${background('low', 0)} 0%,
+              ${background('low', 1)} 100%
+            );
+
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 40%;
+            z-index: ${appearance.index.low};
+          `}
+        />
+      </div>
       <About />
     </>
   )
