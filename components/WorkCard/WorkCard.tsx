d7b460b50c73a8e32e278c29d68c11f92c27ff91
Andy Hook <hello@andy-hook.co.uk>
2021-02-12 13:49:25 +0000

    WorkGridItem -> WorkCard

---
+++ components/WorkCard/WorkCard.tsx
@@ -0,0 +1,139 @@
+import React from 'react'
+import { useTheme } from '../../hooks/useTheme/useTheme'
+import { appearance } from '../../style/appearance'
+import {
+  baseText,
+  displayText,
+  setCropAndLineHeight,
+} from '../../style/typography'
+import Icon from '../Icon/Icon'
+import ImageBase from '../ImageBase/ImageBase'
+import InteractionBase from '../InteractionBase/InteractionBase'
+
+export type WorkCardProps = {
+  src: string
+  alt?: string
+  disabled?: boolean
+  title: string
+  description: string
+  width: number
+  height: number
+  href: string
+}
+
+function WorkCard({
+  src,
+  alt,
+  title,
+  disabled,
+  description,
+  width,
+  height,
+  href,
+}: WorkCardProps): JSX.Element {
+  const { foreground, background } = useTheme()
+
+  return (
+    <InteractionBase
+      href={href}
+      offset={0.75}
+      disabled={disabled}
+      css={`
+        display: block;
+      `}
+    >
+      <div
+        css={`
+          position: relative;
+          overflow: hidden;
+          border-radius: ${appearance.radius.base};
+        `}
+      >
+        {disabled && (
+          <>
+            <div
+              css={`
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                position: absolute;
+                top: 2.1em;
+                left: 2.1em;
+
+                ${baseText.size.lg}
+                padding: 0.75em;
+
+                color: ${foreground('extraHigh')};
+
+                border: ${appearance.borderWidth.thick} solid
+                  ${foreground('extraHigh', 0.2)};
+
+                border-radius: ${appearance.radius.circle};
+
+                z-index: ${appearance.index.medium};
+              `}
+            >
+              <Icon name="lock" />
+            </div>
+            <div
+              css={`
+                position: absolute;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+
+                opacity: 0.8;
+
+                background-color: ${background('low')};
+
+                z-index: ${appearance.index.low};
+              `}
+            />
+          </>
+        )}
+
+        <ImageBase
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          scaleRenderFromBp={['sm', 70]}
+        />
+      </div>
+      <div
+        css={`
+          opacity: ${disabled ? 0.4 : 1};
+        `}
+      >
+        <h3
+          css={`
+            ${displayText.weight.semiBold}
+            ${displayText.size.sm}
+              ${setCropAndLineHeight('display', 'tight')}
+
+              color: ${foreground('extraHigh')};
+
+            margin-top: 1.25em;
+            margin-bottom: 0.4em;
+          `}
+        >
+          {title}
+        </h3>
+        <p
+          css={`
+            ${baseText.weight.regular}
+            ${baseText.size.md}
+              ${setCropAndLineHeight('base', 'tight')}
+
+              color: ${foreground('low')};
+          `}
+        >
+          {description}
+        </p>
+      </div>
+    </InteractionBase>
+  )
+}
+
+export default WorkCard
--- components/WorkGrid/WorkGrid.tsx
+++ components/WorkGrid/WorkGrid.tsx
@@ -4,7 +4,7 @@ import { inclusiveDown, inclusiveUp } from '../../style/responsive'
 import LayoutGutter from '../Layout/LayoutGutter'
 import LayoutLimiter from '../Layout/LayoutLimiter'
 import LayoutRow from '../Layout/LayoutRow'
-import WorkGridItem from './WorkGridItem'
+import WorkCard from '../WorkCard/WorkCard'
 
 function WorkGrid(): JSX.Element {
   return (
@@ -51,7 +51,7 @@ function WorkGrid(): JSX.Element {
                     }
                   `}
                 >
-                  <WorkGridItem
+                  <WorkCard
                     src={thumbnailImage.src}
                     disabled={disabled}
                     href={route}
