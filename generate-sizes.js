7dc0f5db445ec98c59e8cd5dd111149235cce280
Andy Hook <hello@andy-hook.co.uk>
2021-02-26 15:17:48 +0000

    Add image size generation script

---
+++ generate-sizes.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const glob = require('glob')
+const sizeOf = require('image-size')
+
+const imagesDir = './public/images/'
+const files = glob.sync(imagesDir + '/**/*.*')
+
+const images = Object.assign(
+  {},
+  ...Object.keys(files).map((index) => {
+    const sourcePath = files[index]
+
+    try {
+      const dimensions = sizeOf(sourcePath)
+      const filename = sourcePath.replace(imagesDir, '')
+
+      return {
+        [filename]: {
+          filename,
+          width: dimensions.width,
+          height: dimensions.height,
+        },
+      }
+    } catch (err) {
+      console.error(`${sourcePath} – Failed to retrieve image sizes`)
+      console.error(err)
+    }
+  })
+)
+
+fs.writeFileSync('./data/image-sizes.json', JSON.stringify(images))
+
+console.log('Image sizes generated', images)
