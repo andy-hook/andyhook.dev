57a6b53f9561b17dae1fe72244288ac6ebb06a30
Andy Hook <hello@andy-hook.co.uk>
2021-03-04 13:28:01 +0000

    Minor naming adjustment

--- components/ImageBase/ImageBase.tsx
+++ components/ImageBase/ImageBase.tsx
@@ -61,11 +61,11 @@ function ImageBase({
   }, [scaleRender, scaleRenderFromBp])
 
   const handleLoadFromCache = useCallback(
-    (ref) => {
-      // We are unable to set a ref to the underlying image element direct so we must access it via querySelector on the wrapper
+    (wrapperRef) => {
+      // We are unable to set a ref to the underlying image element directly so we must access it via querySelector on the wrapper
       // We are querying by srcset attribute to differentiate from the placeholder image that next/image adds to reserve space
       // https://github.com/vercel/next.js/discussions/18386
-      const image = ref?.querySelector('img[srcset]') as HTMLImageElement
+      const image = wrapperRef?.querySelector('img[srcset]') as HTMLImageElement
 
       if (image && image.complete && showLoader) {
         setShowLoader(false)
