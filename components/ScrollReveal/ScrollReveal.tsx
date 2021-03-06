dc99bc1bdeea2e21cbad185e3ac4074ef349ebe2
Andy Hook <hello@andy-hook.co.uk>
2021-03-06 18:18:51 +0000

    Scale motion range by viewport size

--- components/ScrollReveal/ScrollReveal.tsx
+++ components/ScrollReveal/ScrollReveal.tsx
@@ -1,34 +1,43 @@
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { spring } from '../../style/motion'
+import { useBreakpoint } from 'styled-breakpoints/react-styled'
+import { inclusiveDown } from '../../style/responsive'
 
 type ScrollRevealProps = {
   children: React.ReactNode
 }
 
 function ScrollReveal({ children }: ScrollRevealProps): JSX.Element {
+  const useRelativeYDistance = useBreakpoint(inclusiveDown('xl'))
+
   const [ref, inView] = useInView({
     triggerOnce: true,
     rootMargin: '-15%',
     initialInView: true,
   })
 
+  const variants = useMemo(
+    () => ({
+      hidden: {
+        opacity: 0,
+        y: useRelativeYDistance ? '5vw' : 100,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    }),
+    [useRelativeYDistance]
+  )
+
   return (
     <motion.div
       ref={ref}
       initial="visible"
       animate={inView ? 'visible' : 'hidden'}
-      variants={{
-        hidden: {
-          opacity: 0,
-          y: 100,
-        },
-        visible: {
-          opacity: 1,
-          y: 0,
-        },
-      }}
+      variants={variants}
       transition={spring.snappy}
     >
       {children}
