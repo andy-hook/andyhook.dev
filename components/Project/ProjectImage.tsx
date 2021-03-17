5c1101cf8e3f7f8e5aaa8d28fc689b2b1f89d9ff
Andy Hook <hello@andyhook.dev>
2021-03-17 11:41:19 +0000

    Improve image scaling in Chrome (#106)
    * Improve image quality and performance
    
    * Remove redundant asset
    
    * Tidy comment

--- components/Project/ProjectImage.tsx
+++ components/Project/ProjectImage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ImageProperties } from '../../data/images'
 import ImageBase from '../ImageBase/ImageBase'
-import ScrollReveal from '../ScrollReveal/ScrollReveal'
 
 type ProjectImageProps = ImageProperties & {
   loadingColor?: string
@@ -13,13 +12,7 @@ function ProjectImage({
   alt,
 }: ProjectImageProps): JSX.Element {
   return (
-    <ScrollReveal>
-      <ImageBase
-        imagePath={imagePath}
-        alt={alt}
-        backgroundColor={loadingColor}
-      />
-    </ScrollReveal>
+    <ImageBase imagePath={imagePath} alt={alt} backgroundColor={loadingColor} />
   )
 }
 
