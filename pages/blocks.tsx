ede4578a01515cbb9b3ff4220c2d4b8f47b78406
Andy Hook <hello@andy-hook.co.uk>
2021-03-18 14:11:57 +0000

    Add external content links

--- pages/blocks.tsx
+++ pages/blocks.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from '../components/Link/Link'
 import ProjectDescription from '../components/Project/ProjectDescription'
 import ProjectImageGroup from '../components/Project/ProjectImageGroup'
 import ProjectQuote from '../components/Project/ProjectQuote'
@@ -39,17 +40,24 @@ function BlocksPage(): JSX.Element {
 
       <ProjectDescription title="Decentralized technology takes some practice">
         <TextParagraph>
-          Blocks was originally using Web3.js and a Metamask provider to fetch
-          block information, this was the simplest way to get started but had
-          big drawbacks – primarily the need to have a wallet installed and
-          connected before being able to fetch data. Migrating to Ethers.js
-          offered a more intuitive, opinionated API while using Infura, Alchemy
-          and Etherscan nodes solved the wallet provider problem. That said,
-          many of these concepts were new to me, and while the typical Web2
-          stack is mature and fast, Web3 initially felt cumbersome and difficult
-          to use, which is only made more obvious from the proliferation of
-          excellently documented, low cost data services available with the
-          Former.
+          Blocks was originally using{' '}
+          <Link href="https://web3js.readthedocs.io/">Web3.js</Link> and a{' '}
+          <Link href="https://docs.metamask.io/guide/ethereum-provider.html">
+            MetaMask provider
+          </Link>{' '}
+          to fetch block information, this was the simplest way to get started
+          but had big drawbacks – primarily the need to have a wallet installed
+          and connected before being able to fetch data. Migrating to{' '}
+          <Link href="https://docs.ethers.io/">Ethers.js</Link> offered a more
+          intuitive, opinionated API while using{' '}
+          <Link href="https://infura.io/">Infura</Link>,{' '}
+          <Link href="https://www.alchemyapi.io/">Alchemy</Link> and{' '}
+          <Link href="https://etherscan.io/">Etherscan</Link> backed nodes
+          solved the wallet provider problem. That said, many of these concepts
+          were new to me, and while the typical centralised stack is mature and
+          fast, Web3 initially felt cumbersome and difficult to use, which is
+          only made more obvious from the proliferation of excellently
+          documented, low cost data services available with the former.
         </TextParagraph>
 
         <TextParagraph>
@@ -58,18 +66,25 @@ function BlocksPage(): JSX.Element {
           data massaging and manipulation when a centralised backend service
           would normally handle this work. I found myself having to be careful
           not to carelessly perform calculations with standard Number types and
-          make ample use of BigNumber formatting libraries to guard against safe
-          range throws.
+          make ample use of Big Number formatting libraries to guard against{' '}
+          <Link href="https://docs.ethers.io/v5/api/utils/bignumber/#BigNumber--notes-safenumbers">
+            safe range errors
+          </Link>
+          .
         </TextParagraph>
 
         <TextParagraph>
-          Tech moves fast though, and we now have fantastic projects such as The
-          Graph offering a data abstraction later powered by Subgraphs and
-          GraphQL, we even have native support for BigInt directly in the
-          browser, greatly reducing bundle sizes and standardising around a
-          single API. It's these types of learnings and exposure to new
-          technology that make investing in personal projects rewarding and
-          valuable.
+          Tech moves fast though, and we now have fantastic projects such as{' '}
+          <Link href="https://thegraph.com/">The Graph</Link> offering a data
+          abstraction later powered by Subgraphs and GraphQL, we even have
+          native support for{' '}
+          <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt">
+            BigInt
+          </Link>{' '}
+          directly in the browser , greatly reducing bundle sizes and
+          standardising around a single API. It's these types of learnings and
+          exposure to new technology that make investing in personal projects
+          rewarding and valuable.
         </TextParagraph>
       </ProjectDescription>
 
