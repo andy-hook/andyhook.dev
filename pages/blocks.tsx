5f97c4bd0ddd894dedef3e65ddbdc77363849398
Andy Hook <hello@andy-hook.co.uk>
2021-03-18 13:50:42 +0000

    Add Blocks page content

--- pages/blocks.tsx
+++ pages/blocks.tsx
@@ -13,29 +13,23 @@ function BlocksPage(): JSX.Element {
     <WorkTemplate name="blocks">
       <ProjectDescription title="Overview" bordered>
         <TextParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          aliquet elit eu dui congue pulvinar. Nullam enim quam, lacinia sit
-          amet est a, venenatis accumsan eros. Pellentesque interdum ex velit,
-          in vulputate arcu vulputate sed. Fusce ac suscipit enim. tincidunt
-          quam.
+          I designed and built the first version of Blocks in early 2020 as part
+          of the technical assessment process at Aragon, I wanted to design a
+          tightly scoped app that was visually exciting and offered
+          opportunities for interesting interactions. While the utility of the
+          app is not comparable to the best of existing tools, it still gave me
+          the opportunity to learn more about the Ethereum and experiment with
+          some new front-end techniques.
         </TextParagraph>
 
         <TextParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          aliquet elit eu dui congue pulvinar. Nullam enim quam, lacinia sit
-          amet est a, venenatis accumsan eros. Pellentesque interdum ex velit,
-          in vulputate arcu vulputate sed. Fusce ac suscipit enim. Integer at
-          justo euismod, sodales lectus quis, ullamcorper dui. Nulla justo nibh,
-          tincidunt eu lorem vel, pretium fermentum purus.
-        </TextParagraph>
-
-        <TextParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          aliquet elit eu dui congue pulvinar. Nullam enim quam, lacinia sit
-          amet est a, venenatis accumsan eros. Pellentesque interdum ex velit,
-          in vulputate arcu vulputate sed. Fusce ac suscipit enim. Integer at
-          justo euismod, sodales lectus quis, ullamcorper dui. Nulla justo nibh,
-          tincidunt eu lorem vel, pretium fermentum purus.
+          An interesting byproduct of this project was being able to receive
+          critique and feedback on my implementation during the assessment, this
+          effectively turned the process into an additional knowledge stream and
+          provided learnings which I could cycle back into the code, further
+          improving it for my own needs. While I invested time above and beyond
+          what is typical, I found it exciting to work on and helpful for
+          professional growth.
         </TextParagraph>
       </ProjectDescription>
 
@@ -43,39 +37,46 @@ function BlocksPage(): JSX.Element {
         <ProjectImageGroup.Item imagePath="blocks-intro.jpg" alt="" />
       </ProjectImageGroup>
 
-      <ProjectDescription title="Experimenting with fetching data from the blockchain">
+      <ProjectDescription title="Decentralized technology takes some practice">
         <TextParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          aliquet elit eu dui congue pulvinar. Nullam enim quam, lacinia sit
-          amet est a, venenatis accumsan eros. Pellentesque interdum ex velit,
-          in vulputate arcu vulputate sed. Fusce ac suscipit enim. Integer at
-          justo euismod, sodales lectus quis, ullamcorper dui. Nulla justo nibh,
-          tincidunt eu lorem vel, pretium fermentum purus.
+          Blocks was originally using Web3.js and a Metamask provider to fetch
+          block information, this was the simplest way to get started but had
+          big drawbacks – primarily the need to have a wallet installed and
+          connected before being able to fetch data. Migrating to Ethers.js
+          offered a more intuitive, opinionated API while using Infura, Alchemy
+          and Etherscan nodes solved the wallet provider problem. That said,
+          many of these concepts were new to me, and while the typical Web2
+          stack is mature and fast, Web3 initially felt cumbersome and difficult
+          to use, which is only made more obvious from the proliferation of
+          excellently documented, low cost data services available with the
+          Former.
         </TextParagraph>
 
         <TextParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          aliquet elit eu dui congue pulvinar. Nullam enim quam, lacinia sit
-          amet est a, venenatis accumsan eros. Pellentesque interdum ex velit,
-          in vulputate arcu vulputate sed. Fusce ac suscipit enim. Integer at
-          justo euismod, sodales lectus quis, ullamcorper dui. Nulla justo nibh,
-          tincidunt eu lorem vel, pretium fermentum purus.
+          Another aspect I had to consider was that interacting with a chain
+          directly from a client requires that the front-end handles much of the
+          data massaging and manipulation when a centralised backend service
+          would normally handle this work. I found myself having to be careful
+          not to carelessly perform calculations with standard Number types and
+          make ample use of BigNumber formatting libraries to guard against safe
+          range throws.
         </TextParagraph>
 
         <TextParagraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          aliquet elit eu dui congue pulvinar. Nullam enim quam, lacinia sit
-          amet est a, venenatis accumsan eros. Pellentesque interdum ex velit,
-          in vulputate arcu vulputate sed. Fusce ac suscipit enim. Integer at
-          justo euismod, sodales lectus quis, ullamcorper dui. Nulla justo nibh,
-          tincidunt eu lorem vel, pretium fermentum purus.
+          Tech moves fast though, and we now have fantastic projects such as The
+          Graph offering a data abstraction later powered by Subgraphs and
+          GraphQL, we even have native support for BigInt directly in the
+          browser, greatly reducing bundle sizes and standardising around a
+          single API. It's these types of learnings and exposure to new
+          technology that make investing in personal projects rewarding and
+          valuable.
         </TextParagraph>
       </ProjectDescription>
 
       <ProjectImageGroup
         framed
-        frameGradientStart="#ECA35F"
-        frameGradientEnd="#B44634"
+        frameGradientStart="#d88350"
+        frameGradientEnd="#b64a32"
         loadingColor="#181F23"
       >
         <ProjectImageGroup.Item imagePath="blocks-home.jpg" alt="" />
