4ace759b916218897e9648a02e1b779ca839a242
Andy Hook <hello@andy-hook.co.uk>
2021-04-05 16:30:58 +0100

    [TopBar] Apply visibility logic

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -11,6 +11,10 @@ import {
 import { inclusiveUp } from '../../style/responsive'
 import Logo from '../Logo/Logo'
 import Menu from '../Menu/Menu'
+import { useScrollPosition } from '../../hooks/useScrollPosition/useScrollPosition'
+
+const VISIBILITY_OFFSET_PX = 1500
+const RAISED_EFFECT_OFFSET_PX = 5
 
 const containerXPadding = css`
   padding-left: 1.25rem;
@@ -35,14 +39,32 @@ const containerXPadding = css`
 function TopBar(): JSX.Element {
   const theme = useTheme()
   const motionVariants = useRelativeYMotion(-ENTRANCE_TRANSITION_Y_DISTANCE / 2)
+  const { current } = useScrollPosition()
+
+  const raisedEffect = current.y.offset > RAISED_EFFECT_OFFSET_PX
+  const visible =
+    (current.y.offset > VISIBILITY_OFFSET_PX &&
+      current.y.direction === 'backward') ||
+    current.y.offset <= VISIBILITY_OFFSET_PX
 
   return (
-    <div
+    <motion.div
+      animate={{
+        transform: visible ? 'translateY(0%)' : 'translateY(-100%',
+      }}
+      transition={spring.tactile}
       css={`
         position: fixed;
         top: 0;
         left: 0;
+
         width: 100%;
+
+        height: 3.75rem;
+
+        ${inclusiveUp('sm')} {
+          height: 4.4rem;
+        }
 
         z-index: ${theme.index.high};
       `}
@@ -54,35 +76,51 @@ function TopBar(): JSX.Element {
         animate="visible"
         css={`
           ${containerXPadding}
-          display: flex;
 
-          align-items: center;
-          justify-content: space-between;
-
-          border-bottom: ${theme.borderWidth.regular} solid
-            ${theme.background('extraHigh')};
-
-          background-color: ${theme.background('low')};
-
-          height: 3.75rem;
-
-          ${inclusiveUp('sm')} {
-            height: 4.4rem;
-          }
+          height: 100%;
         `}
       >
         <div
           css={`
+            position: relative;
             display: flex;
             align-items: center;
+            justify-content: space-between;
+            height: 100%;
+
+            z-index: ${theme.index.medium};
           `}
         >
           <Logo />
+
+          <Menu />
         </div>
 
-        <Menu />
+        <motion.div
+          animate={{ opacity: raisedEffect ? 1 : 0 }}
+          transition={spring.punchy}
+          css={`
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+
+            z-index: ${theme.index.floor};
+            background: linear-gradient(
+              5deg,
+              ${theme.background('medium')} 0%,
+              ${theme.background('low')} 50%
+            );
+
+            box-shadow: ${theme.shadow.low};
+
+            border-bottom: ${theme.borderWidth.regular} solid
+              ${theme.background('extraHigh')};
+          `}
+        />
       </motion.header>
-    </div>
+    </motion.div>
   )
 }
 
