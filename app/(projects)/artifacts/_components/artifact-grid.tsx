335b915c4efc3b72e3d1dba41d28d1689a50d1af
Andy Hook <hello@andyhook.dev>
2026-08-07 16:53:40 +0100

    Grid

--- app/(projects)/artifacts/_components/artifact-grid.tsx
+++ app/(projects)/artifacts/_components/artifact-grid.tsx
@@ -24,10 +24,10 @@ export const ArtifactGrid = React.forwardRef<ArtifactGridElement, ArtifactGridPr
           'md:[--gap:30px]',
           'lg:[--gap:40px]',
           'xl:[--gap:50px]',
-          'widest:[--gap:70px]',
           'grid-cols-[repeat(auto-fill,_minmax(100%,_1fr))]',
           'sm:grid-cols-[repeat(auto-fill,_minmax(50%,_1fr))]',
           'wide:grid-cols-[repeat(auto-fill,_minmax(33%,_1fr))]',
+          'max:grid-cols-[repeat(auto-fill,_minmax(25%,_1fr))]',
           'grid m-[calc(-1_*_var(--gap)_/_2)]',
           className,
         )}
@@ -95,7 +95,15 @@ export const ArtifactGridFrame = React.forwardRef<ArtifactGridFrameElement, Arti
         style={{ inset: 'calc(var(--gap) / 2)', ...props.style }}
         ref={forwardedRef}
       >
-        <div className={cx('absolute', 'inset-[6vw]', 'sm:inset-[3vw]', 'wide:inset-[1.5vw]')}>
+        <div
+          className={cx(
+            'absolute',
+            'inset-[6vw]',
+            'sm:inset-[3vw]',
+            'wide:inset-[1.5vw]',
+            'max:inset-[1.25vw]',
+          )}
+        >
           <StickyLabel.Chip label={label} />
           {children}
         </div>
